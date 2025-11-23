7814e758f7dd4fa9f8ffa4ad8458dc19a9a7a57a
Rostyslav Svoikin <honrok261@gmail.com>
2025-11-23 20:55:55 +0200

    was added some logic in lesson 4

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,21 +4,55 @@
 //	Console.WriteLine(i + 1);
 //}
 
-Console.WriteLine("Write numbers and I will sum all the numbers before you send me 0");
-int sum = 0;
-int res = 0;
-int counter = 1;
+//Console.WriteLine("Write numbers and I will sum all the numbers before you send me 0");
 
-while (counter != 0){
-	string input = Console.ReadLine();
-	if (input != null && int.TryParse(input, out int number))
+//int sum = 0;
+//int res = 0;
+//int counter = 1;
+
+//while (counter != 0){
+//	string input = Console.ReadLine();
+//	if (input != null && int.TryParse(input, out int number))
+//	{
+//		res = int.Parse(input);
+//		sum += res;
+//		counter++;
+//		Console.WriteLine(sum);
+//	}
+//	else {
+//		counter = 0;
+//	}
+//}
+
+bool exit = false;
+
+while (!exit)
+{
+	Console.WriteLine("Вибери пункт меню:");
+	Console.WriteLine("1. Привітання");
+	Console.WriteLine("2. Показати дату");
+	Console.WriteLine("3. Вийти");
+
+	string choice = Console.ReadLine();
+
+	if (choice == "1")
 	{
-		res = int.Parse(input);
-		sum += res;
-		counter++;
-		Console.WriteLine(sum);
+		Console.WriteLine("Привіт!");
 	}
-	else {
-		counter = 0;
+	else if (choice == "2")
+	{
+		Console.WriteLine("Сьогодні: " + DateTime.Now.ToShortDateString());
 	}
+	else if (choice == "3")
+	{
+		Console.WriteLine("Вихід...");
+		exit = true; // вихід з циклу
+	}
+	else
+	{
+		Console.WriteLine("Невірний вибір, спробуй ще раз.");
+	}
+
+	Console.WriteLine(); // пустий рядок для краси
 }
+
