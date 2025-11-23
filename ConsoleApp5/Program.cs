dd9504b44d52b71d87187a94eed8913397dfac4b
Rostyslav Svoikin <honrok261@gmail.com>
2025-11-23 21:16:15 +0200

    first commit in 5 lesson

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿//int[] numbers = new	int[10];
+
+//Console.WriteLine(numbers[2]);
+
+
+//int[] numbers = {1, 2, 3, 4, 5};
+
+using System.Collections.Generic;
+ List<string> names = new List<string>();
+names.Add("Rost");
+names.Add("Ania");
+
+foreach (string name in names) { 
+	Console.WriteLine(name);
+}
