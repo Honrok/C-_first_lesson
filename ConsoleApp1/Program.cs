3a7c28cc9a9a4f4aa1f2fc4b627bf8718cc39306
Rostyslav Svoikin <honrok261@gmail.com>
2025-11-23 18:21:46 +0200

    First lesson commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿string name = "Rost";
+Console.WriteLine(name);
+string answer = Console.ReadLine();
+
+checkLetter();
+void checkLetter() { 
+	if (answer != null)
+	{
+		Console.WriteLine(answer);
+	}
+}
+
