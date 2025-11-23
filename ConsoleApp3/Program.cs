f967a41a39fc8906350fc16de88d92adc996ddc2
Rostyslav Svoikin <honrok261@gmail.com>
2025-11-23 19:42:33 +0200

    lesson3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿int age;
+
+
+Console.WriteLine("Tell me your age");
+string input = Console.ReadLine();
+
+if (input != null && int.TryParse(input, out int number))
+{
+	age = int.Parse(input);
+	if (age >= 18)
+	{
+		Console.WriteLine("Повнолітній");
+	}
+	else
+	{
+		Console.WriteLine("fuck " + age);
+	}
+}
+else
+{
+	Console.WriteLine("Ти ввів якусь хуйню");
+}
+
+
