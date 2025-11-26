8c39b58b82452d049cbb8e50a2d47e6e9ad01843
Rostyslav Svoikin <honrok261@gmail.com>
2025-11-26 17:35:42 +0200

    lesson7

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+public class Person(string name, int age) { 
+	private string personName = name;
+	private int personAge = age;
+
+	public void SayHello() {
+		Console.WriteLine($"Hello {personName} and your age is {personAge}");
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Person first = new Person("Rost", 28);
+		first.SayHello();
+	}
+}
+
+
+
