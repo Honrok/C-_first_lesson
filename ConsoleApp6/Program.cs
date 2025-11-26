0a1d612844742c014c12945c82d56da9f9a02e1d
Rostyslav Svoikin <honrok261@gmail.com>
2025-11-26 16:44:52 +0200

    Lesson6

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,62 @@
+﻿//Console.WriteLine("Tell me your name");
+//string nameInput = Console.ReadLine();
+//SayHello(nameInput);
+//static void SayHello(string name) {
+//	Console.WriteLine("Hello " + name);
+
+//}
+//Console.WriteLine(Sum(10, 3));
+//static int Sum(int a, int b) {
+//	return a + b;
+//}
+
+
+//Console.WriteLine(IfEvenNumber(24));
+//static bool IfEvenNumber(double number) {
+//	Console.WriteLine(number%2);
+//	if (number % 2 != 0)
+//	{
+//		return false;
+//	}
+//	else {
+//		return true;
+//	}
+//}
+
+
+//bool val = false;
+//while (!val) {
+//	Console.WriteLine("1. ADD\r\n2. SUB\r\n3. MUL\r\n4. DIV\r\n5. EXIT");
+//	string input = Console.ReadLine();
+//	if (input == "1")
+//	{
+//		Console.WriteLine("Put me 2 numbers");
+//		string firstNum = Console.ReadLine();
+//		string secNum = Console.ReadLine();
+//		Console.WriteLine("The Sum of these numbers is " + (double.Parse(firstNum) + double.Parse(secNum)));
+//	}
+//	else if (input == "2")
+//	{
+//		Console.WriteLine("Put me 2 numbers");
+//		string firstNum = Console.ReadLine();
+//		string secNum = Console.ReadLine();
+//		Console.WriteLine("The SUB of these numbers is " + (double.Parse(firstNum) - double.Parse(secNum)));
+//	}
+//	else if (input == "3")
+//	{
+//		Console.WriteLine("Put me 2 numbers");
+//		string firstNum = Console.ReadLine();
+//		string secNum = Console.ReadLine();
+//		Console.WriteLine("The MUL of these numbers is " + (double.Parse(firstNum) * double.Parse(secNum)));
+//	}
+//	else if (input == "4")
+//	{
+//		Console.WriteLine("Put me 2 numbers");
+//		string firstNum = Console.ReadLine();
+//		string secNum = Console.ReadLine();
+//		Console.WriteLine("The DIV of these numbers is " + (double.Parse(firstNum) / double.Parse(secNum)));
+//	} else if (input == "5") {
+//		val = true;
+//	}
+
+//}
