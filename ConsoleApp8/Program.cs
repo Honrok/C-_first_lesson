2b2478cfb01eaf5b1a2a7e309f59e9ee6db88402
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-02 14:59:44 +0200

    inheritance

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿class Animal {
+	public string name = "";
+	public void Eat() {
+		Console.WriteLine($"{name} eats");
+	}
+}
+
+class Dog : Animal {
+	public void Bark() {
+		Console.WriteLine($"{name} barks");
+	}
+}
+
+
+class Program {
+	public static void Main()
+	{
+		Dog dog1 = new Dog();
+
+
+		dog1.name = "Rex";
+		dog1.Eat();
+		dog1.Bark();
+	}
+}
