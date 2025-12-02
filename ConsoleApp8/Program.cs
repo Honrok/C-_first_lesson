39fde5e6a4dae317821f500ce3ef701a84d616ab
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-02 15:44:24 +0200

    was added abstract classes

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -24,38 +24,60 @@
 //	}
 //}
 
-class Animal {
-	public string name = "";
-	public virtual void MakeSound() {
-		Console.WriteLine("Animal makes sound");
+//class Animal {
+//	public string name = "";
+//	public virtual void MakeSound() {
+//		Console.WriteLine("Animal makes sound");
+//	}
+//}
+
+//class Dog : Animal
+//{
+//	public override void MakeSound()
+//	{
+//		Console.WriteLine("GAV GAV");
+//	}
+//}
+
+//class Cat : Animal
+//{
+//    public override void MakeSound()
+//    {
+//		Console.WriteLine("Miu Miu");
+//    }
+//}
+
+//	class Program {
+//	public static void Main() {
+
+//		Dog dog1 = new Dog();
+//		Cat cat1 = new Cat();
+
+//		dog1.MakeSound();
+//		cat1.MakeSound();
+
+//	}
+
+//	}
+
+
+abstract class Shape {
+	public abstract double Area();
+}
+class Circle : Shape {
+
+	public double Radius;
+	public override double Area() {
+		double res = Math.PI* Radius *Radius;
+		return res;
 	}
 }
-
-class Dog : Animal
-{
-	public override void MakeSound()
-	{
-		Console.WriteLine("GAV GAV");
-	}
-}
-
-class Cat : Animal
-{
-    public override void MakeSound()
-    {
-		Console.WriteLine("Miu Miu");
-    }
-}
-
-	class Program {
-	public static void Main() {
-
-		Dog dog1 = new Dog();
-		Cat cat1 = new Cat();
-
-		dog1.MakeSound();
-		cat1.MakeSound();
-
-	}
+class Program {
+	public static void Main(string[] args) {
+		Circle circle1 = new Circle();
+		circle1.Radius = 11.1;
+		Console.WriteLine(circle1.Area());
 		
-	}
+	}	
+
+}
