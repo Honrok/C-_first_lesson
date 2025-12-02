6cb58479218bacc33bcdcf11cc1849e6b8ae73e5
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-02 16:17:40 +0200

    was encapsulation

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,24 @@
+﻿class Person { 
+	private int _age;
+	public int Age{
+		get { return _age; }
+		set {
+			if (value >= 0)
+			{
+				_age = value;
+			}
+			else {
+				Console.WriteLine("This is imposible");
+			}
+		}
+	}
+}
+
+class Program {
+	public static void Main(string[] args) {
+		Person pers1 = new Person();
+		pers1.Age = -2;
+		pers1.Age = 10;
+		Console.WriteLine(pers1.Age);
+	}
+}
