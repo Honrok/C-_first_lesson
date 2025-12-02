a7a90e195ddd568afa03d32ef87f3a5327b658f7
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-02 15:53:03 +0200

    was added description for lesson

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,4 +1,6 @@
-﻿//class Animal {
+﻿//Наслідування, це коли ми можемо створювати класи, які наслідують батьківський клас і всі його меотди. При наявності конструктора, також наслідуємо обовязкові параметри
+
+//class Animal {
 //	public string name = "";
 //	public void Eat() {
 //		Console.WriteLine($"{name} eats");
@@ -23,6 +25,11 @@
 //		dog1.Bark();
 //	}
 //}
+
+
+
+//Поліморфізм, це коли різні обєкти класу реалізують метод цього класу по різному.
+
 
 //class Animal {
 //	public string name = "";
@@ -60,24 +67,37 @@
 
 //	}
 
+//Неможливо створити екземпляр абстрактного класу, тільки наслідувати
 
-abstract class Shape {
-	public abstract double Area();
-}
-class Circle : Shape {
+//abstract class Shape {
+//	public abstract double Area();
+//}
+//class Circle : Shape {
 
-	public double Radius;
-	public override double Area() {
-		double res = Math.PI* Radius *Radius;
-		return res;
-	}
-}
-class Program {
-	public static void Main(string[] args) {
-		Circle circle1 = new Circle();
-		circle1.Radius = 11.1;
-		Console.WriteLine(circle1.Area());
-		
-	}	
+//	public double Radius;
+//	public override double Area() {
+//		double res = Math.PI* Radius *Radius;
+//		return res;
+//	}
+//}
+//class Program {
+//	public static void Main(string[] args) {
+//		Circle circle1 = new Circle();
+//		circle1.Radius = 11.1;
+//		Console.WriteLine(circle1.Area());
 
-}
+//	}	
+
+//}
+
+
+//інтерфейси задають класи, які похідні класи повинні обовязково реалізувати
+//interface IFly {
+//	void Fly();
+//}
+
+//class Bird : IFly {
+//	public void Fly() {
+//		Console.WriteLine("I'm bird");
+//	}
+//}
