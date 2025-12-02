55f4ff67cbc75015ab7e2d86c605be33a5781750
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-02 15:20:55 +0200

    Polymorphism

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,25 +1,61 @@
-﻿class Animal {
+﻿//class Animal {
+//	public string name = "";
+//	public void Eat() {
+//		Console.WriteLine($"{name} eats");
+//	}
+//}
+
+//class Dog : Animal {
+//	public void Bark() {
+//		Console.WriteLine($"{name} barks");
+//	}
+//}
+
+
+//class Program {
+//	public static void Main()
+//	{
+//		Dog dog1 = new Dog();
+
+
+//		dog1.name = "Rex";
+//		dog1.Eat();
+//		dog1.Bark();
+//	}
+//}
+
+class Animal {
 	public string name = "";
-	public void Eat() {
-		Console.WriteLine($"{name} eats");
+	public virtual void MakeSound() {
+		Console.WriteLine("Animal makes sound");
 	}
 }
 
-class Dog : Animal {
-	public void Bark() {
-		Console.WriteLine($"{name} barks");
-	}
-}
-
-
-class Program {
-	public static void Main()
+class Dog : Animal
+{
+	public override void MakeSound()
 	{
-		Dog dog1 = new Dog();
-
-
-		dog1.name = "Rex";
-		dog1.Eat();
-		dog1.Bark();
+		Console.WriteLine("GAV GAV");
 	}
 }
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+		Console.WriteLine("Miu Miu");
+    }
+}
+
+	class Program {
+	public static void Main() {
+
+		Dog dog1 = new Dog();
+		Cat cat1 = new Cat();
+
+		dog1.MakeSound();
+		cat1.MakeSound();
+
+	}
+		
+	}
