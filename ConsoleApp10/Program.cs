6319f17c5ee69acf6de664535bec49e32fce977b
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-09 13:58:46 +0200

    lesson10, but 11, what is static and how it use

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,34 @@
+﻿//class Player {
+//	string Name;
+//	static int PlayersCount = 0;
+
+//	public Player(){
+//		PlayersCount++;
+//		Console.WriteLine(PlayersCount);
+//	}
+//}
+
+//class Program {
+
+//	public static void Main(string[] args) {
+//		Player pl1 = new();
+//		Player pl2 = new();
+//		Player pl3 = new();
+//	}
+//}
+
+//using System.Numerics;
+
+//static class MathUtils {
+//	public static int Multiply(int a, int b) {
+//		return (a * b);
+//	}
+//}
+
+//class Program {
+//	public static void Main(string[] args) {
+//		//MathUtils m1 = new();
+//		int sum = MathUtils.Multiply(3, 5);
+//		Console.WriteLine(sum);
+//	}
+//}
