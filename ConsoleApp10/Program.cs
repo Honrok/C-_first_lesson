f3776d39c677d633b4729967eab6a000c1d29150
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-09 15:52:53 +0200

    what static means

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -32,3 +32,27 @@
 //		Console.WriteLine(sum);
 //	}
 //}
+
+//class Car {
+//	private static int _countOfCars = 0;
+//	public static int CountOfCars {
+//		get {
+//			return _countOfCars;
+//		}
+//	}
+//	public Car()
+//	{
+//		_countOfCars++;
+//		Console.WriteLine(_countOfCars);
+
+//	}
+//}
+//class Program {
+//	public static void Main(string[] args) {
+//		Car car1 = new();
+//		Car car2 = new();
+//		Car car3 = new();
+//		Car car4 = new();
+//		Console.WriteLine(Car.CountOfCars);
+//	}
+//}
