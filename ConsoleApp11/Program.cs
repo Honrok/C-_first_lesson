94771d32522b37596a878078e3028e67a740d394
Rostyslav Svoikin <honrok261@gmail.com>
2025-12-09 16:12:22 +0200

    enum and stract

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,45 @@
+﻿//enum GamesStates { 
+//	Menu,
+//	Playing,
+//	Paused,
+//	GameOver
+//}
+
+//class Program {
+//	public static void Main(string[] args) {
+//		GamesStates state = GamesStates.Menu;
+
+//		if (state == GamesStates.Playing)
+//		{
+//			Console.WriteLine("Player is playing");
+//		}
+//		else {
+//			Console.WriteLine("Player is in menu");
+//		}
+//	}
+//}
+
+//enum PlayersState { 
+//	Idle,
+//	Run,
+//	Jump,
+//	Attack
+//}
+
+//struct Vector2D { 
+//	public int x;
+//	public int y;
+//}
+
+//class Program {
+//	public static void Main(string[] args) {
+//		Vector2D val1 = new ();
+
+//		val1.y = 1;
+//		val1.x = 15;
+//		Console.WriteLine (val1);	
+//		Console.WriteLine (val1.y);	
+//		Console.WriteLine (val1.x);	
+
+//	}
+//}
